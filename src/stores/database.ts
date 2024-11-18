de4474eb97e70c629caf161b73ef0aa8714da3e4
Bartek Paczesny <bartek@paczesny.pl>
2024-11-18 03:02:15 +0100

    forgot abt version change...(i should use some kind of tool for autoincrement)

--- src/stores/database.ts
+++ src/stores/database.ts
@@ -3,7 +3,7 @@ import Database from "@tauri-apps/plugin-sql";
 // Initialize the database connection
 export const initializeStore = async () => {
   try {
-    const db = await Database.load("sqlite:content-manager.db");
+    const db = await Database.load("sqlite:content-planner.db");
     return db;
   } catch (error) {
     console.error("Error loading the database:", error);
