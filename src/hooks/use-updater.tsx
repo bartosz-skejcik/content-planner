35a7e29fdcdef66aaa400bf05b22f1dc17cf8d21
Bartek Paczesny <bartek@paczesny.pl>
2024-11-21 14:24:14 +0100

    chore(release): fix updater

--- src/hooks/use-updater.tsx
+++ src/hooks/use-updater.tsx
@@ -1,79 +1,72 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { check } from "@tauri-apps/plugin-updater";
-import { ask, message } from "@tauri-apps/plugin-dialog";
 import { invoke } from "@tauri-apps/api/core"; // Import Tauri invoke
+import { ToastAction } from "@/components/ui/toast";
 
 // Custom hook for handling updates
-export const useUpdater = () => {
+export const useUpdater = (toast: any) => {
   const [progress, setProgress] = useState(0);
   const [showProgressDialog, setShowProgressDialog] = useState(false);
 
-  const checkForUpdates = useCallback(async (onUserClick = false) => {
-    try {
-      // Check for updates
-      const update = await check();
-      if (update === null) {
-        await message("Failed to check for updates.\nPlease try again later.", {
-          title: "Error",
-          kind: "error",
-          okLabel: "OK",
-        });
-        return;
-      } else if (update?.available) {
-        const userConfirmed = await ask(
-          `Update to ${update.version} is available!\n\nRelease notes: ${update.body}`,
-          {
-            title: "Update Available",
-            kind: "info",
-            okLabel: "Update",
-            cancelLabel: "Cancel",
-          },
-        );
-        if (userConfirmed) {
-          setShowProgressDialog(true);
+  const checkForUpdates = async () => {
+    // Check for updates
+    const update = await check();
+    if (update === null) {
+      toast({
+        title: "Failed to check for updates",
+        description: "Please try again later.",
+        variant: "destructive",
+      });
+      return;
+    } else if (update?.available) {
+      toast({
+        title: "Update Available",
+        description: `Version ${update.version} is available!`,
+        action: (
+          <ToastAction
+            onClick={async () => {
+              try {
+                setShowProgressDialog(true);
 
-          let downloaded = 0;
-          let contentLength = 0;
+                let downloaded = 0;
+                let contentLength = 0;
 
-          await update.downloadAndInstall((event) => {
-            switch (event.event) {
-              case "Started":
-                contentLength = event.data.contentLength || 0;
-                break;
-              case "Progress":
-                downloaded += event.data.chunkLength;
-                const percentage = (downloaded / contentLength) * 100;
-                setProgress(percentage);
-                break;
-              case "Finished":
-                setProgress(100);
-                break;
-            }
-          });
+                await update.downloadAndInstall((event) => {
+                  switch (event.event) {
+                    case "Started":
+                      contentLength = event.data.contentLength || 0;
+                      break;
+                    case "Progress":
+                      downloaded += event.data.chunkLength;
+                      const percentage = (downloaded / contentLength) * 100;
+                      setProgress(percentage);
+                      break;
+                    case "Finished":
+                      setProgress(100);
+                      break;
+                  }
+                });
 
-          setShowProgressDialog(false);
-          // After the update is downloaded and installed, gracefully restart the app
-          await invoke("graceful_restart");
-        }
-      } else if (onUserClick) {
-        await message("You are on the latest version. Stay awesome!", {
-          title: "No Update Available",
-          kind: "info",
-          okLabel: "OK",
-        });
-      }
-    } catch (error) {
-      console.error("Error during update check:", error);
-      await message(
-        "An error occurred while checking for updates. Please try again later.",
-        {
-          title: "Error",
-          kind: "error",
-          okLabel: "OK",
-        },
-      );
+                setShowProgressDialog(false);
+                // After the update is downloaded and installed, gracefully restart the app
+                await invoke("graceful_restart");
+              } catch (error) {
+                console.error("Error during update check:", error);
+                toast({
+                  title: "An error occurred while checking for updates",
+                  description: "Please try again later.",
+                  variant: "destructive",
+                });
+              }
+            }}
+            altText="Update"
+          >
+            Update
+          </ToastAction>
+        ),
+      });
     }
-  }, []);
+  };
 
   return { checkForUpdates, progress, showProgressDialog };
 };
